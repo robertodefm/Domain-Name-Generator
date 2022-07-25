4ca39663874405934e4d7b685a52de93f1beae5f
robertodefm <robertodefm@gmail.com>
2022-07-25 19:29:07 +0000

    ok

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ var adj = ['great', 'big' ];
 var noun = ['jogger','racoon'];
 var ext = [".com", ".net", ".us", ".io"]
 
-for (let i=0; i<pronoun.length; i++){
-    for (let j=0; j<adj.length; j++){
-        for (let k=0; k<noun.length; k++){
-            for (let l=0; l<ext.length; l++){
+for (i=0; i<pronoun.length; i++){
+    for (j=0; j<adj.length; j++){
+        for (k=0; k<noun.length; k++){
+            for (l=0; l<ext.length; l++){
             console.log(pronoun[i] + adj[j] + noun[k] + ext[l])
             }
         }
